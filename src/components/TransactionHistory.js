c62d5f4afdaf6906d729bf424641d260188c95e2
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-05-28 19:44:51 +0300

    modified

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TransactionHistory = ({ items }) => (
   <table className="transaction-history">
@@ -20,5 +21,16 @@ const TransactionHistory = ({ items }) => (
     </tbody>
   </table>
 );
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
+};
 
 export default TransactionHistory;
