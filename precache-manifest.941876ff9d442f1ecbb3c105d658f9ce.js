b7a87dadeef7d475c94fc3dc6b6890c2865ab0b9
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-06-08 21:22:09 +0300

    Updates

---
+++ precache-manifest.941876ff9d442f1ecbb3c105d658f9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519719f3f79d10570f87adb35ed4b473",
+    "url": "/goit-react-hw-01/./index.html"
+  },
+  {
+    "revision": "b1638b466b2c0235881f",
+    "url": "/goit-react-hw-01/./static/css/main.02c15ab0.chunk.css"
+  },
+  {
+    "revision": "99e74b348478ac03cf09",
+    "url": "/goit-react-hw-01/./static/js/2.72463725.chunk.js"
+  },
+  {
+    "revision": "b1638b466b2c0235881f",
+    "url": "/goit-react-hw-01/./static/js/main.89ee4ad0.chunk.js"
+  },
+  {
+    "revision": "6a0183f26732c2f9a297",
+    "url": "/goit-react-hw-01/./static/js/runtime~main.62bc8f36.js"
+  },
+  {
+    "revision": "d5a2ccba3b615528e1ab6a953e7c06bb",
+    "url": "/goit-react-hw-01/./static/media/01.d5a2ccba.jpg"
+  }
+]);
